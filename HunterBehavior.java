080038f78f652b2b4ba194adc0e14b879be4e271
N1known-LCDR <victorpalmqvist16@gmail.com>
2026-05-09 09:03:26 +0200

    Delete HunterBehavior.java

--- HunterBehavior.java
+++
@@ -1,8 +0,0 @@
-package dk.sdu.imada.oop26;
-
-public class HunterBehavior implements GhostBehavior{
-    @Override
-    public void move(Ghost ghost, Player player, Map map){
-        ghost.moveTowards(player.getRow(), player.getCol());
-    }
-}
