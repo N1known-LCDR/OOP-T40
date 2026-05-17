0f65dc9a821ad60ab58e9bb2aa0744064d708f97
Ricky Pagh <ripag25@student.sdu.dk>
2026-05-17 11:05:03 +0200

    hunterbehaviour comments
    Added comment to explain movement behavior.

--- template/pacman/src/main/java/dk/sdu/imada/oop26/HunterBehavior.java
+++ template/pacman/src/main/java/dk/sdu/imada/oop26/HunterBehavior.java
@@ -1,8 +1,10 @@
 package dk.sdu.imada.oop26;
 
+// Behaviour to hunt down the player
 public class HunterBehavior implements GhostBehavior{
     @Override
     public void move(Ghost ghost, Player player, Map map){
+        // Moves towards the player using Breadth-first search
         ghost.moveTowards(player.getRow(), player.getCol());
     }
 }
