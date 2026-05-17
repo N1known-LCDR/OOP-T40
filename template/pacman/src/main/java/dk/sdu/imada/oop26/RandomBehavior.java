09751aabe61280b16dc7a170923044bd0b0c160d
Ricky Pagh <ripag25@student.sdu.dk>
2026-05-17 11:18:08 +0200

    Add comment to move method in RandomBehavior

--- template/pacman/src/main/java/dk/sdu/imada/oop26/RandomBehavior.java
+++ template/pacman/src/main/java/dk/sdu/imada/oop26/RandomBehavior.java
@@ -3,6 +3,7 @@ package dk.sdu.imada.oop26;
 public class RandomBehavior implements GhostBehavior{
     @Override
     public void move(Ghost ghost, Player player, Map map){
+        // Move in a random direction
         ghost.moveRandom();
     }
 }
